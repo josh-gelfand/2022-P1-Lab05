a6f116f5d962f8c92a45a5107ec18440eda80c36
12154 <joshua.gelfand@vtsd.com>
2022-10-31 09:49:46 -0400

    Lab 5 JG

--- Lab05vst.java
+++ Lab05vst.java
@@ -6,17 +6,55 @@ import java.awt.*;
 import java.applet.*;
 
 
-public class Lab05vst extends Applet
-{
-    public void paint(Graphics g)
-    {
+public class Lab05vst extends Applet {
+    public void paint(Graphics g) {
         int width = 980;
         int height = 630;
         int x1 = 10;
         int y1 = 640;
         int x2 = 990;
         int y2 = 640;
-        g.drawRect(10,10,width,height);
+        g.drawRect(10, 10, width, height);
+
+        for (int y = 1; y < 30; y++) {
+            g.drawLine(x1, y1, x2, y2);
+            x1 += 32.6;
+            y2 -= 21.3;
+        }
+
+        x1 = 10;
+        y1 = 640;
+        x2 = 990;
+        y2 = 640;
+        for (int y = 1; y < 30; y++) {
+            g.drawLine(x2, y2, x1, y1);
+            y1 -= 21.3;
+            x2 -= 32.6;
+        }
+
+        x1 = 10;
+        y1 = 10;
+        x2 = 990;
+        y2 = 10;
+        for (int y = 1; y < 30; y++) {
+            g.drawLine(x1, y1, x2, y2);
+            y1 += 21.3;
+            x2 -= 32.6;
+        }
+        x1 = 990;
+        y1 = 10;
+        x2 = 10;
+        y2 = 10;
+        for (int y = 1; y < 30; y++) {
+            g.drawLine(x1, y1, x2, y2);
+            y1 += 21.3;
+            x2 += 32.6;
+        }
     }
 }
+
+
+
+
+
 
